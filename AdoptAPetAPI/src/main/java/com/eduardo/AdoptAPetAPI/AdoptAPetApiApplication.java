78678d5ee3d130e13771cd4e512b20a3b6717548
Eduardo Araujo <eduardo1996araujo@gmail.com>
2024-01-03 19:31:13 -0300

    First commit

---
+++ AdoptAPetAPI/src/main/java/com/eduardo/AdoptAPetAPI/AdoptAPetApiApplication.java
@@ -0,0 +1,13 @@
+package com.eduardo.AdoptAPetAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptAPetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptAPetApiApplication.class, args);
+	}
+
+}
