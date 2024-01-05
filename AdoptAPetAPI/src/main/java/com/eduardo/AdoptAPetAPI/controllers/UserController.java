3cd4d9db2c79c63254b0e6209571a7d5c629f8ca
Eduardo Araujo <eduardo1996araujo@gmail.com>
2024-01-04 22:23:19 -0300

    Método create user

--- AdoptAPetAPI/src/main/java/com/eduardo/AdoptAPetAPI/controllers/UserController.java
+++ AdoptAPetAPI/src/main/java/com/eduardo/AdoptAPetAPI/controllers/UserController.java
@@ -1,4 +1,31 @@
 package com.eduardo.AdoptAPetAPI.controllers;
 
+import com.eduardo.AdoptAPetAPI.dto.UserDTO;
+import com.eduardo.AdoptAPetAPI.exceptions.UserAlreadyRegisteredException;
+import com.eduardo.AdoptAPetAPI.services.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserController {
+
+    private final UserService service;
+
+    @PostMapping
+    public ResponseEntity<UserDTO> registerUser(@Valid @RequestBody UserDTO userDto) {
+        userDto = service.registerUser(userDto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(userDto.getId()).toUri();
+        return ResponseEntity.created(uri).body(userDto);
+    }
 }
--- AdoptAPetAPI/src/main/java/com/eduardo/AdoptAPetAPI/services/UserService.java
+++ AdoptAPetAPI/src/main/java/com/eduardo/AdoptAPetAPI/services/UserService.java
@@ -1,17 +1,33 @@
 package com.eduardo.AdoptAPetAPI.services;
 
+import com.eduardo.AdoptAPetAPI.converter.UserConverter;
+import com.eduardo.AdoptAPetAPI.dto.UserDTO;
 import com.eduardo.AdoptAPetAPI.entities.User;
+import com.eduardo.AdoptAPetAPI.exceptions.UserAlreadyRegisteredException;
 import com.eduardo.AdoptAPetAPI.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+    private final UserConverter converter;
 
-    public User registerUser(User user){
+    public UserDTO registerUser(UserDTO userDTO)  {
+        verifyIfIsAlreadyRegistered(userDTO.getEmail());
+        User userToSave = converter.toEntity(userDTO);
+        User savedUser = userRepository.save(userToSave);
+        return converter.toDTO(savedUser);
+    }
 
+    private void verifyIfIsAlreadyRegistered(String email){
+        Optional<User> optSavedUser = userRepository.findByEmail(email);
+        if (optSavedUser.isPresent()){
+            throw new UserAlreadyRegisteredException(String.format("O email %s já está registrado.",email));
+        }
     }
 }
