04343c8eb1c8847ed7aac3a7cf2448a525e925b2
Eduardo Araujo <eduardo1996araujo@gmail.com>
2024-01-09 21:37:59 -0300

    Adicionado documentação swagger

--- AdoptAPetAPI/src/main/java/com/eduardo/AdoptAPetAPI/controllers/AnimalController.java
+++ AdoptAPetAPI/src/main/java/com/eduardo/AdoptAPetAPI/controllers/AnimalController.java
@@ -2,6 +2,10 @@ package com.eduardo.AdoptAPetAPI.controllers;
 
 import com.eduardo.AdoptAPetAPI.dto.AnimalDTO;
 import com.eduardo.AdoptAPetAPI.services.AnimalService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -14,9 +18,15 @@ import java.net.URI;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("animals")
+@Tag(name = "animals", description = "Api para gerenciamento de animais")
 public class AnimalController {
 
     private final AnimalService service;
+
+    @Operation(summary = "Registra um novo animal e o abrigo onde ele está", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Animal registrado com sucesso.")
+    })
 
     @PostMapping
     public ResponseEntity<AnimalDTO> registerAnimalForAdoption(@Valid @RequestBody AnimalDTO dto){
@@ -26,11 +36,21 @@ public class AnimalController {
         return ResponseEntity.created(uri).body(animalDto);
     }
 
+    @Operation(summary = "Retornar lista paginada dos animais", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista retornada com sucesso")
+    })
+
     @GetMapping("/all")
     public ResponseEntity<Page<AnimalDTO>> getAll(@RequestParam (defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size){
         Page<AnimalDTO> returnAll = service.findAll(page, size);
         return ResponseEntity.ok().body(returnAll);
     }
+
+    @Operation(summary = "Adotar um animal", method = "DELETE")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Animal adotado com sucesso")
+    })
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> adoptAnimal(@PathVariable Long id){
--- AdoptAPetAPI/src/main/java/com/eduardo/AdoptAPetAPI/controllers/UserController.java
+++ AdoptAPetAPI/src/main/java/com/eduardo/AdoptAPetAPI/controllers/UserController.java
@@ -3,6 +3,10 @@ package com.eduardo.AdoptAPetAPI.controllers;
 import com.eduardo.AdoptAPetAPI.dto.UserDTO;
 import com.eduardo.AdoptAPetAPI.exceptions.UserAlreadyRegisteredException;
 import com.eduardo.AdoptAPetAPI.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -16,9 +20,17 @@ import java.net.URI;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
+@Tag(name = "users", description = "Api para gerenciamento de usuários")
 public class UserController {
 
     private final UserService service;
+
+    @Operation(summary = "Registra um novo usuário", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Usuário criado com sucesso"),
+            @ApiResponse(responseCode = "409", description = "Email já cadastrado")
+
+    })
 
     @PostMapping
     public ResponseEntity<UserDTO> registerUser(@Valid @RequestBody UserDTO userDto) {
@@ -28,17 +40,36 @@ public class UserController {
         return ResponseEntity.created(uri).body(userDto);
     }
 
+    @Operation(summary = "Busca um usuário pelo email", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuário encontrado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado")
+    })
+
     @GetMapping("/{email}")
     public ResponseEntity<UserDTO> findByEmail(@PathVariable String email) {
         var userDto = service.findByEmail(email);
         return ResponseEntity.ok().body(userDto);
     }
 
+    @Operation(summary = "Lista paginada de usuários", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista com todos usuarios cadastrados")
+    })
+
     @GetMapping("/paginated")
-    public ResponseEntity<Page<UserDTO>> getUsersPaginated(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
+    public ResponseEntity<Page<UserDTO>> getUsersPaginated(@RequestParam(defaultValue = "0") int page,
+                                                           @RequestParam(defaultValue = "10") int size) {
         Page<UserDTO> returnUsers = service.findAll(page, size);
         return ResponseEntity.ok().body(returnUsers);
     }
+
+    @Operation(summary = "Deletar um usuário", method = "DELETE")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Usuário deletado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida"),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado"),
+    })
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
